0f9330be431a7509857f51d118fc698cca4962bc
Bityacht-Developer <55692615+dennislee928@users.noreply.github.com>
2024-05-20 20:27:50 +0800

    kick start
    note:
    use "extends": ["next/babel","next/core-web-vitals"] in .eslintrc.json

---
+++ frontstage/app/components/Navbar/Navbar.jsx
@@ -0,0 +1,32 @@
+import React, { useState } from "react";
+import "./Navbar.css";
+
+function Navbar() {
+  const [isOpen, setIsOpen] = useState(false);
+
+  return (
+    <nav className="navbar">
+      <div className="brand-title">Brand</div>
+      <a href="#" className="toggle-button" onClick={() => setIsOpen(!isOpen)}>
+        <span className="bar"></span>
+        <span className="bar"></span>
+        <span className="bar"></span>
+      </a>
+      <div className={`navbar-links ${isOpen ? "active" : ""}`}>
+        <ul>
+          <li>
+            <a href="/">首頁</a>
+          </li>
+          <li>
+            <a href="/authors">作者</a>
+          </li>
+          <li>
+            <a href="/contributors">貢獻者</a>
+          </li>
+        </ul>
+      </div>
+    </nav>
+  );
+}
+
+export default Navbar;
